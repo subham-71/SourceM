ec339c7c21e221cb96e583638d60ce4a995bb37b
Parth Jain <parthljain2002@gmail.com>
2023-04-14 15:31:37 +0530

    Auth context

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
           <section class="flex flex-col md:flex-row h-screen items-center">
 
               <div class="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
-                  <img src="dash.avif" alt="" class="w-full h-full"/>
+                  <img src="dash.jpg" alt="" class="w-full h-full"/>
               </div>
 
               <div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
