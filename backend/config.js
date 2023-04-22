696921d9817be6e9ab7654ff2e86755c210498de
subham <subhamsubhasis2002@gmail.com>
2023-04-22 14:53:59 +0530

    firebase config

--- backend/config.js
+++ backend/config.js
@@ -5,32 +5,26 @@ const assert = require('assert');
 dotenv.config();
 
 const {
-    PORT,
-    HOST,
-    HOST_URL,
     API_KEY,
     AUTH_DOMAIN,
-    DATABASE_URL,
     PROJECT_ID,
     STORAGE_BUCKET,
     MESSAGING_SENDER_ID,
-    APP_ID
+    APP_ID,
+    MEASUREMENT_ID
 } = process.env;
 
-assert(PORT, 'PORT is required');
-assert(HOST, 'HOST is required');
+
 
 module.exports = {
-    port: PORT,
-    host: HOST,
-    url: HOST_URL,
+
     firebaseConfig: {
         apiKey: API_KEY,
         authDomain: AUTH_DOMAIN,
-        databaseURL: DATABASE_URL,
         projectId: PROJECT_ID,
         storageBucket: STORAGE_BUCKET,
         messagingSenderId: MESSAGING_SENDER_ID,
-        appId: APP_ID
+        appId: APP_ID,
+        measurementId: MEASUREMENT_ID
     }
 }
