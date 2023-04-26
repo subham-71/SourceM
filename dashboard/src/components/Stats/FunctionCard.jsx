a8da4c6bd59d4d56cb6d4687ea791831c1b24a02
Parth Jain <parthljain2002@gmail.com>
2023-04-26 14:25:45 +0530

    Light Mode Changes

--- dashboard/src/components/Stats/FunctionCard.jsx
+++ dashboard/src/components/Stats/FunctionCard.jsx
@@ -13,20 +13,20 @@ return (
 
         <>
          
-        <div class="mx-auto bg-white rounded-lg md:p-8 dark:bg-gray-800" id="statistics" role="tabpanel" aria-labelledby="statistics-tab">
+        <div class="mx-auto rounded-lg md:p-8 bg-gray-800" id="statistics" role="tabpanel" aria-labelledby="statistics-tab">
               <div> HI </div>
-              <dl class="grid grid-cols-2 gap-8 p-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-3 dark:text-white sm:p-8">
+              <dl class="grid grid-cols-2 gap-8 p-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-3 text-white sm:p-8">
                   <div class="flex flex-col text-center">
                       <dt class="mb-2 text-3xl font-extrabold">{executionTime}</dt>
-                      <dd class="text-gray-500 dark:text-gray-400">Execution Time</dd>
+                      <dd class="text-gray-400">Execution Time</dd>
                   </div>
                   <div class="flex flex-col text-center">
                       <dt class="mb-2 text-3xl font-extrabold">{executionCount}</dt>
-                      <dd class="text-gray-500 dark:text-gray-400">Function Calls</dd>
+                      <dd class="text-gray-400">Function Calls</dd>
                   </div>
                   <div class="flex flex-col text-center">
                       <dt class="mb-2 text-3xl font-extrabold">1000 GB </dt>
-                      <dd class="text-gray-500 dark:text-gray-400">Memory Consumption</dd>
+                      <dd class="text-gray-400">Memory Consumption</dd>
                   </div>
               </dl>
               
