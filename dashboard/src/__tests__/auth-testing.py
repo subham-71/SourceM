1133b37a3c255f1071c97013baac88728dccb7cc
PJ02 <parthljain2002@gmail.com>
2023-05-04 04:44:20 +0530

    Added Invalid Login

---
+++ dashboard/src/__tests__/auth-testing.py
@@ -0,0 +1,85 @@
+# import all required frameworks
+import unittest
+import selenium
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class AuthTest(unittest.TestCase):
+
+    # initialization of webdriver
+    def setUp(self):
+        self.driver= webdriver.Chrome()
+        self.driver.get("https://sourcem.netlify.app/")
+
+    def test_check_landing_page(self):
+        
+        # get driver
+        driver = self.driver
+        driver.get("https://sourcem.netlify.app/")
+        time.sleep(2)
+        assert driver.title == "SourceM"
+
+    def test_check_login(self):
+        driver = self.driver
+        driver.get("https://sourcem.netlify.app/login")
+
+        time.sleep(5)
+
+        # Fill the login form
+        # Xpath is //input[@type="email"]
+        username = driver.find_element(By.XPATH, "//input[@type='email']")
+        username.send_keys("parth3@gmail.com")
+        password = driver.find_element(By.XPATH, "//input[@type='password']")
+        password.send_keys("parth123")
+
+        # Click the login button
+        login = driver.find_element(By.XPATH, "//button[1]")
+        login.click()
+
+        time.sleep(2)
+
+        # Check if the user is redirected to the dashboard
+        print(driver.current_url)
+        assert driver.current_url == "https://sourcem.netlify.app/dashboard"
+
+    def test_check_invalid_login(self):
+        driver = self.driver
+        driver.get("https://sourcem.netlify.app/login")
+
+        time.sleep(5)
+
+        # Fill the login form
+        username = driver.find_element(By.XPATH, "//input[@type='email']")
+        username.send_keys("invalid@gmail.com")
+        password = driver.find_element(By.XPATH, "//input[@type='password']")
+        password.send_keys("invalid")
+
+        # Click the login button
+        login = driver.find_element(By.XPATH, "//button[1]")
+        login.click()
+
+        time.sleep(2)
+
+        # selenium.common.exceptions.UnexpectedAlertPresentException: Alert Text: Failed to Log in
+        # Message: unexpected alert open: {Alert text : Failed to Log in}
+
+        # Catch the exception
+        try:
+            alert = driver.switch_to.alert
+            alert.accept()
+        except:
+            pass
+
+        # Check if the user is still on the login page
+        assert driver.current_url == "https://sourcem.netlify.app/login"
+
+
+    # cleanup method called after every test performed
+    def tearDown(self):
+        self.driver.close()
+
+
+# execute the script
+if __name__ == "__main__":
+    unittest.main()
