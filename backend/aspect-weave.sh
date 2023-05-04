d1f6ad2894b8ed1ac4e998c3fe87b24f7772d4ba
PJ02 <parthljain2002@gmail.com>
2023-05-04 20:08:43 +0530

    Cleanup Changes

--- backend/aspect-weave.sh
+++ backend/aspect-weave.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+rm *.tar
+rm *.zip
+rm -r outputs
+
 mv config_data/input.jar .
 
 ajc -source 1.8 -cp "./ReqJars/aspectjrt.jar:./ReqJars/gson-2.10.1.jar" -sourceroots "./AOPDeps/" -injars ./input.jar -outjar output.jar
