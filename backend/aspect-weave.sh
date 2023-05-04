d441792ba05dca28ef06456d0400b4e54e4e662d
PJ02 <parthljain2002@gmail.com>
2023-05-04 20:00:23 +0530

    Bash script changes

--- backend/aspect-weave.sh
+++ backend/aspect-weave.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 mv config_data/input.jar .
+
 ajc -source 1.8 -cp "./ReqJars/aspectjrt.jar:./ReqJars/gson-2.10.1.jar" -sourceroots "./AOPDeps/" -injars ./input.jar -outjar output.jar
 mkdir outputs
+
 mv output.jar outputs/
-cp ./DemoJars/aspectjrt.jar outputs/
-cp ./DemoJars/gson-2.10.1.jar outputs/
+mv config_data/config.properties outputs/
+cp ./ReqJars/aspectjrt.jar outputs/
+cp ./ReqJars/gson-2.10.1.jar outputs/
+
 rm *.jar
 tar -cvf output.tar outputs/
+zip -r output.zip outputs/
 
